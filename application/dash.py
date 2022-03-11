da261406a34df4b007e3cd127531d631f66d4024
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2022-03-10 19:17:22 -0600

    Update dash.py
    actualizado enero22

--- application/dash.py
+++ application/dash.py
@@ -30,8 +30,8 @@ d2 = today.strftime("Fecha de actualización : %d-%m-%Y")
 tabla1 = pd.read_csv('https://raw.githubusercontent.com/fdealbam/violenciadegenero/main/Tabla1.csv')              
 tabla1_f = tabla1[tabla1['Tipo de delito']== 'Corrupción de menores' ]
 tabla1_f.reset_index(inplace=True,)
-TOTCORRUPMENORES = tabla1_f.iloc[0]['GRAND TOTAL']
-TASACORRUPMENORES = tabla1_f.iloc[0]['tasa_acumulada']
+#TOTCORRUPMENORES = tabla1_f.iloc[0]['GRAND TOTAL']
+#TASACORRUPMENORES = tabla1_f.iloc[0]['tasa_acumulada']
 
 ###############################
 # DATABASES
@@ -59,6 +59,7 @@ year18= fem[fem.Año == 2018]
 year19= fem[fem.Año == 2019]
 year20= fem[fem.Año == 2020]
 year21= fem[fem.Año == 2021]
+year22= fem[fem.Año == 2022]
 
 ############################################### Agregar suffix de años
 
@@ -90,6 +91,9 @@ y21= year21.add_suffix('21')
 y21.rename(columns ={'Año21': 'Año', 'Tipo de delito21': 'Tipo de delito','Unnamed: 021' : 'Unnamed: 0',
                             'Entidad21': 'Entidad'}, inplace = True)
 
+y22= year22.add_suffix('22')
+y22.rename(columns ={'Año22': 'Año', 'Tipo de delito22': 'Tipo de delito','Unnamed: 022' : 'Unnamed: 0',
+                            'Entidad22': 'Entidad'}, inplace = True)
 
 
 ############################################### Concat todos los años
@@ -100,8 +104,9 @@ fc = fb.merge(y18, on="Entidad",  how="inner")
 fd = fc.merge(y19, on="Entidad",  how="inner")
 fe = fd.merge(y20, on="Entidad",  how="inner")
 ff = fe.merge(y21, on="Entidad",  how="inner")
+fg = ff.merge(y22, on="Entidad",  how="inner")
                     
-femi15_21 = ff[[
+femi15_21 = fg [[
  'Entidad','Enero15','Febrero15','Marzo15','Abril15','Mayo15','Junio15',
  'Julio15','Agosto15','Septiembre15','Octubre15','Noviembre15','Diciembre15',
  
@@ -121,7 +126,10 @@ femi15_21 = ff[[
  'Agosto20','Septiembre20','Octubre20','Noviembre20','Diciembre20',
     
  'Enero21','Febrero21','Marzo21','Abril21','Mayo21','Junio21','Julio21',
- 'Agosto21','Septiembre21','Octubre21','Noviembre21','Diciembre21'
+ 'Agosto21','Septiembre21','Octubre21','Noviembre21','Diciembre21',
+    
+ 'Enero22', #'Febrero22', 'Marzo22','Abril22', 'Mayo22', 'Junio22', 'Julio22', 'Agosto22',
+   #'Septiembre22','Octubre22','Noviembre22','Diciembre22'
              ]]
 
 
@@ -145,12 +153,13 @@ femi15_21['Total2019']= femi15_21[[ 'Enero19', 'Febrero19', 'Marzo19', 'Abril19'
 femi15_21['Total2020']= femi15_21[[ 'Enero20', 'Febrero20', 'Marzo20', 'Abril20', 'Mayo20',
                                'Junio20', 'Julio20', 'Agosto20', 'Septiembre20', 'Octubre20',
                                'Noviembre20', 'Diciembre20',]].sum(axis=1)
-
 femi15_21['Total2021']= femi15_21[[ 'Enero21','Febrero21', 'Marzo21', 'Abril21', 'Mayo21',
                                    'Junio21','Julio21','Agosto21','Septiembre21','Octubre21',
-                                   'Noviembre21','Diciembre21'
+                                   'Noviembre21','Diciembre21']].sum(axis=1)
+femi15_21['Total2022']= femi15_21[[ 'Enero22',# 'Febrero22', 'Marzo22', 'Abril22', 'Mayo22',
+                              # 'Junio22', 'Julio22', 'Agosto22', 'Septiembre22', 'Octubre22',
+                               #'Noviembre22', 'Diciembre22',
                                   ]].sum(axis=1)
-
 
 #identificadores
 conf_2015= femi15_21.Total2015.sum().astype(int)
@@ -160,11 +169,12 @@ conf_2018= femi15_21.Total2018.sum().astype(int)
 conf_2019= femi15_21.Total2019.sum().astype(int)
 conf_2020= femi15_21.Total2020.sum().astype(int)
 conf_2021= femi15_21.Total2021.sum().astype(int)
+conf_2022= femi15_21.Total2022.sum().astype(int)
 
 
 
 ################################################## PREPARA GRAFICA MENSUAL
-pagra = ff[[
+pagra = fg[[
   'Enero15', 'Febrero15', 'Marzo15', 'Abril15', 'Mayo15', 'Junio15', 'Julio15', 'Agosto15', 
     'Septiembre15', 'Octubre15', 'Noviembre15', 'Diciembre15',
  
@@ -183,7 +193,11 @@ pagra = ff[[
     'Septiembre20','Octubre20', 'Noviembre20', 'Diciembre20',
 
  'Enero21', 'Febrero21', 'Marzo21','Abril21', 'Mayo21', 'Junio21','Julio21', 'Agosto21',
-    'Septiembre21','Octubre21','Noviembre21','Diciembre21'
+    'Septiembre21','Octubre21','Noviembre21','Diciembre21',
+    
+    'Enero22',# 'Febrero22', 'Marzo22','Abril22', 'Mayo22', 'Junio22', 'Julio22', 'Agosto22',
+   #'Septiembre22','Octubre22','Noviembre22','Diciembre22'
+    
             ]]
 
 
@@ -263,12 +277,12 @@ fem_filter1['Total']=fem_filter1['Total'].astype(int)
 
 junto1 = pd.read_csv('https://raw.githubusercontent.com/fdealbam/feminicidios/main/application/POB_15_21.csv')
 fem15_21 = femi15_21[['Entidad', 'Total2015', 'Total2016', 'Total2017',
-       'Total2018', 'Total2019', 'Total2020', 'Total2021']]
+       'Total2018', 'Total2019', 'Total2020', 'Total2021','Total2022']]
 
 junto15_21 = fem15_21.merge(junto1,right_on='NOM_ENT',left_on='Entidad')
 junto15_21["Entidad"].replace('Veracruz de Ignacio de la Llave','Veracruz' , inplace=True)
 
-junto15_21['Totfem1521']=junto15_21[['Total2015', 'Total2016', 'Total2017', 'Total2018','Total2019', 'Total2020', 'Total2021']].sum(1)
+junto15_21['Totfem1521']=junto15_21[['Total2015', 'Total2016', 'Total2017', 'Total2018','Total2019', 'Total2020', 'Total2021','Total2022']].sum(1)
 junto15_21['Totpob1521']=junto15_21[['POB15', 'POB16', 'POB17', 'POB18','POB19', 'POB20', 'POB21']].sum(1)
 junto15_21['Tasa1521']=((junto15_21.Totfem1521/junto15_21.Totpob1521)*100000).round(2)
 
@@ -326,6 +340,126 @@ graf_totfem.update_layout(
   #  height=600
     )
 
+
+delito = delitos.copy()
+delito.replace(np.nan,0, inplace=True)
+delito.groupby(['Entidad','Municipio','Cve. Municipio'])['Enero', 'Febrero', 'Marzo','Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre','Noviembre',
+                                        'Diciembre'].sum().to_csv('0agrup.csv')
+delitoso = pd.read_csv('0agrup.csv')
+delitoso['Grand total'] = delitoso[['Enero', 'Febrero', 'Marzo','Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre','Noviembre', 'Diciembre']].sum(1)
+TOTCORRUPMENORES = delitoso['Grand total'].sum()
+pobtot = junto15_21['Totpob1521'].sum()
+TASACORRUPMENORES = round((TOTCORRUPMENORES/pobtot)*100000,0)
+
+otrooso = delitoso.copy()
+otrooso.groupby(['Entidad'])['Grand total'].sum().to_csv('0agrup2.csv')
+enorden = pd.read_csv('0agrup2.csv')
+enorden2 = enorden.sort_values('Grand total', ascending=False, ignore_index=True)
+
+edoname1 = enorden2.iloc[0]['Entidad']
+edoname2 = enorden2.iloc[1]['Entidad']
+edoname3 = enorden2.iloc[2]['Entidad']
+edoname4 = enorden2.iloc[3]['Entidad']
+
+
+delCiu = delitoso[delitoso.Entidad == edoname1 ]
+delMex = delitoso[delitoso.Entidad == edoname2 ]
+delChi = delitoso[delitoso.Entidad == edoname3 ]
+delPue = delitoso[delitoso.Entidad == edoname4 ]
+
+delCiu2 = delCiu.sort_values('Grand total', ascending=False, ignore_index=True)
+delMex2 = delMex.sort_values('Grand total', ascending=False, ignore_index=True)
+delChi2 = delChi.sort_values('Grand total', ascending=False, ignore_index=True)
+delPue2 = delPue.sort_values('Grand total', ascending=False, ignore_index=True)
+
+n1edo1 = delCiu2.iloc[0]['Municipio']
+n2edo1 = delCiu2.iloc[1]['Municipio']
+n3edo1 = delCiu2.iloc[2]['Municipio']
+n4edo1 = delCiu2.iloc[3]['Municipio']
+n5edo1 = delCiu2.iloc[4]['Municipio']
+n6edo1 = delCiu2.iloc[5]['Municipio']
+n7edo1 = delCiu2.iloc[6]['Municipio']
+#n8edo1 = delCiu2.iloc[7]['Municipio']
+#n9edo1 = delCiu2.iloc[8]['Municipio']
+#n10edo1 = delCiu2.iloc[9]['Municipio']
+v1edo1 = int(delCiu2.iloc[0]['Grand total'])
+v2edo1 = int(delCiu2.iloc[1]['Grand total'])
+v3edo1 = int(delCiu2.iloc[2]['Grand total'])
+v4edo1 = int(delCiu2.iloc[3]['Grand total'])
+v5edo1 = int(delCiu2.iloc[4]['Grand total'])
+v6edo1 = int(delCiu2.iloc[5]['Grand total'])
+v7edo1 = int(delCiu2.iloc[6]['Grand total'])
+#v8edo1 = int(delCiu2.iloc[7]['Grand total'])
+#v9edo1 = int(delCiu2.iloc[8]['Grand total'])
+#v10edo1 = int(delCiu2.iloc[9]['Grand total'])
+
+n1edo2 = delMex2.iloc[0]['Municipio']
+n2edo2 = delMex2.iloc[1]['Municipio']
+n3edo2 = delMex2.iloc[2]['Municipio']
+n4edo2 = delMex2.iloc[3]['Municipio']
+n5edo2 = delMex2.iloc[4]['Municipio']
+n6edo2 = delMex2.iloc[5]['Municipio']
+n7edo2 = delMex2.iloc[6]['Municipio']
+n8edo2 = delMex2.iloc[7]['Municipio']
+n9edo2 = delMex2.iloc[8]['Municipio']
+n10edo2 = delMex2.iloc[9]['Municipio']
+v1edo2 = int(delMex2.iloc[0]['Grand total'])
+v2edo2 = int(delMex2.iloc[1]['Grand total'])
+v3edo2 = int(delMex2.iloc[2]['Grand total'])
+v4edo2 = int(delMex2.iloc[3]['Grand total'])
+v5edo2 = int(delMex2.iloc[4]['Grand total'])
+v6edo2 = int(delMex2.iloc[5]['Grand total'])
+v7edo2 = int(delMex2.iloc[6]['Grand total'])
+v8edo2 = int(delMex2.iloc[7]['Grand total'])
+v9edo2 = int(delMex2.iloc[8]['Grand total'])
+v10edo2 =int( delMex2.iloc[9]['Grand total'])
+
+n1edo3 = delChi2.iloc[0]['Municipio']
+n2edo3 = delChi2.iloc[1]['Municipio']
+n3edo3 = delChi2.iloc[2]['Municipio']
+n4edo3 = delChi2.iloc[3]['Municipio']
+n5edo3 = delChi2.iloc[4]['Municipio']
+n6edo3 = delChi2.iloc[5]['Municipio']
+n7edo3 = delChi2.iloc[6]['Municipio']
+n8edo3 = delChi2.iloc[7]['Municipio']
+n9edo3 = delChi2.iloc[8]['Municipio']
+n10edo3 = delChi2.iloc[9]['Municipio']
+v1edo3 = int(delChi2.iloc[0]['Grand total'])
+v2edo3 = int(delChi2.iloc[1]['Grand total'])
+v3edo3 = int(delChi2.iloc[2]['Grand total'])
+v4edo3 = int(delChi2.iloc[3]['Grand total'])
+v5edo3 = int(delChi2.iloc[4]['Grand total'])
+v6edo3 = int(delChi2.iloc[5]['Grand total'])
+v7edo3 = int(delChi2.iloc[6]['Grand total'])
+v8edo3 = int(delChi2.iloc[7]['Grand total'])
+v9edo3 = int(delChi2.iloc[8]['Grand total'])
+v10edo3 =int( delChi2.iloc[9]['Grand total'])
+
+n1edo4 = delPue2.iloc[0]['Municipio']
+n2edo4 = delPue2.iloc[1]['Municipio']
+n3edo4 = delPue2.iloc[2]['Municipio']
+n4edo4 = delPue2.iloc[3]['Municipio']
+n5edo4 = delPue2.iloc[4]['Municipio']
+n6edo4 = delPue2.iloc[5]['Municipio']
+n7edo4 = delPue2.iloc[6]['Municipio']
+n8edo4 = delPue2.iloc[7]['Municipio']
+n9edo4 = delPue2.iloc[8]['Municipio']
+n10edo4 = delPue2.iloc[9]['Municipio']
+v1edo4 = int(delPue2.iloc[0]['Grand total'])
+v2edo4 = int(delPue2.iloc[1]['Grand total'])
+v3edo4 = int(delPue2.iloc[2]['Grand total'])
+v4edo4 = int(delPue2.iloc[3]['Grand total'])
+v5edo4 = int(delPue2.iloc[4]['Grand total'])
+v6edo4 = int(delPue2.iloc[5]['Grand total'])
+v7edo4 = int(delPue2.iloc[6]['Grand total'])
+v8edo4 = int(delPue2.iloc[7]['Grand total'])
+v9edo4 = int(delPue2.iloc[8]['Grand total'])
+v10edo4 =int( delPue2.iloc[9]['Grand total'])
+
+bulletedo1 = ("Las 10 alcaldías con más trata de personas fueron: "+str(n1edo1)  +" ("+ str(v1edo1)+"), "+str(n2edo1) +" ("+ str(v2edo1)+"), "+str(n3edo1) +" ("+ str(v3edo1)+"), "+str(n4edo1) +" ("+ str(v4edo1)+"), "+str(n5edo1) +" ("+ str(v5edo1)+"), "+str(n6edo1) +" ("+ str(v6edo1)+") y "+str(n7edo1) +" ("+ str(v7edo1)+"). ")#+str(n8edo1) +" ("+ str(v8edo1)+"), "+str(n9edo1) +" ("+ str(v9edo1) +") y "+str(n10edo1)+" ("+ str(v10edo1)+").")
+bulletedo2 = ("Los 10 municipios con más trata de personas fueron: "+str(n1edo2) +" ("+ str(v1edo2)+"), "+str(n2edo2) +" ("+ str(v2edo2)+"), "+str(n3edo2) +" ("+ str(v3edo2)+"), "+str(n4edo2) +" ("+ str(v4edo2)+"), "+str(n5edo2) +" ("+ str(v5edo2)+"), "+str(n6edo2) +" ("+ str(v6edo2)+"), "+str(n7edo2) +" ("+ str(v7edo2)+"), "+str(n8edo2) +" ("+ str(v8edo2)+"), "+str(n9edo2) +" ("+ str(v9edo2)+") y "+str(n10edo2) +" ("+ str(v10edo2)+").")
+bulletedo3 = ("Los 10 municipios con más trata de personas fueron: "+str(n1edo3) +" ("+ str(v1edo3)+"), "+str(n2edo3) +" ("+ str(v2edo3)+"), "+str(n3edo3) +" ("+ str(v3edo3)+"), "+str(n4edo3) +" ("+ str(v4edo3)+"), "+str(n5edo3) +" ("+ str(v5edo3)+"), "+str(n6edo3) +" ("+ str(v6edo3)+"), "+str(n7edo3) +" ("+ str(v7edo3)+"), "+str(n8edo3) +" ("+ str(v8edo3)+"), "+str(n9edo3) +" ("+ str(v9edo3)+") y "+str(n10edo3) +" ("+ str(v10edo3)+").")
+bulletedo4 = ("Los 10 municipios con más trata de personas fueron: "+str(n1edo4) +" ("+ str(v1edo4)+"), "+str(n2edo4) +" ("+ str(v2edo4)+"), "+str(n3edo4) +" ("+ str(v3edo4)+"), "+str(n4edo4) +" ("+ str(v4edo4)+"), "+str(n5edo4) +" ("+ str(v5edo4)+"), "+str(n6edo4) +" ("+ str(v6edo4)+"), "+str(n7edo4) +" ("+ str(v7edo4)+"), "+str(n8edo4) +" ("+ str(v8edo4)+"), "+str(n9edo4) +" ("+ str(v9edo4)+") y "+str(n10edo4) +" ("+ str(v10edo4)+").")
 
 
 
@@ -399,7 +533,7 @@ body = html.Div([
                     "La corrupción de menores es uno de los delitos más graves de la violencia de género que se vive en el país, "
                     "además, son problemas aún irresueltos y son tema central de la " 
                     "agenda legislativa, pero hoy alcanzan relevancia en la agenda seguridad pública del país, también. "
-                   " Entre 2015 y 2021 se registraron "+ str(f"{int(TOTCORRUPMENORES):,}") +" casos, lo que representa una tasa de "+
+                   " Entre 2015 y 2022 se registraron "+ str(f"{int(TOTCORRUPMENORES):,}") +" casos, lo que representa una tasa de "+
        str(TASACORRUPMENORES) +" delitos por cada 100 mil habitantes. "+
       "En este tablero analítico observamos su gravedad según intervalos anuales e intervalos mensuales"+
       "; también incluimos un análisis detallado de las cuatro entidades con más incidencias en este delito"+
@@ -514,6 +648,15 @@ body = html.Div([
                          
                         'width': '200px'
                          }, disabled=True)),
+               dbc.Col(dbc.Button(([html.H5("2022", style={"font-size": 18,"color": "black","background-color": "white"}),
+                                    html.H1([str(f"{conf_2022:,d}")],style={"font-size": 45, "color": "black","background-color": "white"}),
+                                    dbc.CardImg(src="https://github.com/fdealbam/Corrupciondemenores/blob/main/application/static/Mapa%20corrupciondemenores%20Total2022.png?raw=true",
+                                                 style={"background-color":"white"}),
+               ]), style={"background-color":"white",
+                         "box-shadow": "10px 20px 30px black",
+                         
+                        'width': '200px'
+                         }, disabled=True)),
                                                         ]),
     
 
@@ -576,8 +719,7 @@ body = html.Div([
                dbc.Col(dbc.Button(([html.P("Baja California", style={"font-size": 30,"color": "black","background-color": "white"}),
                        dbc.CardImg(src="https://github.com/fdealbam/Corrupciondemenores/blob/main/application/static/bc.jpeg?raw=true",
                   style={'size': 2,}),
-                          html.P(
-                          "Los 5 municipios con más corrupción de menores fueron: Tijuana (1,118), Mexicali (793), Ensenada (344), Playas de Rosarito (50) y Tecate (46)",
+                          html.P(bulletedo1,
                      style={'font-size': 14, "font-family":"Arial", "text-align":"justify" }),
                ]), style={"background-color":"white",
                          "box-shadow": "10px 20px 30px black",
@@ -589,10 +731,7 @@ body = html.Div([
                dbc.Col(dbc.Button(([html.P("Ciudad de México", style={"font-size": 30,"color": "black","background-color": "white"}),
                        dbc.CardImg(src="https://github.com/fdealbam/Corrupciondemenores/blob/main/application/static/cdmx.jpeg?raw=true",
                                     style={'size': 2,}),
-                       html.P(
-                           "Las 10 alcaldías con más corrupción de menores fueron: Iztapalapa (220), Cuauhtémoc (183), Gustavo A. Madero (167)"
-                           ", Tlalpan (104), Coyoacán (79), Álvaro Obregón (78), Azcapotzalco (69), Xochimilco (57), Miguel Hidalgo (57)"
-                           " e Iztacalco (55).",
+                       html.P(bulletedo2,
                               style={'font-size': 14, "font-family":"Arial", "text-align":"justify" }),
                        ]), style={"background-color":"white",
                          "box-shadow": "10px 20px 30px black",
@@ -611,11 +750,7 @@ body = html.Div([
           dbc.Col(dbc.Button(([html.P("Guanajuato", style={"font-size": 30,"color": "black","background-color": "white"}),
                        dbc.CardImg(src="https://github.com/fdealbam/Corrupciondemenores/blob/main/application/static/gto.jpeg?raw=true"),
     
-                       html.P(
-                           "Los 10 municipios con más corrupción de menores fueron: León (260), Irapuato (183), Celaya (138), Guanajuato (76)"
-                           ", Dolores Hidalgo Cuna de la Independencia Nacional (43), San Miguel de Allende (36), Salamanca (33)"
-                           ", Cortazar (18), Apaseo el Grande (18) y San Felipe (17)."
-                           " y San Diego de la Unión (0).",
+                       html.P(bulletedo3,
                            style={'font-size': 14, "font-family":"Arial", "text-align":"justify" }),
                ]), style={"background-color":"white",
                          "box-shadow": "10px 20px 30px black",
@@ -630,10 +765,7 @@ body = html.Div([
                                      html.Br(),
                                      html.Br(),
                                    
-                        html.P(
-                          "Los 10 municipios con más corrupción de menores fueron: Monterrey (276), Guadalupe (153), Apodaca (85), Juárez (77)"
-                            ", García (72), San Nicolás de los Garza (67), Santa Catarina (48), Cadereyta Jiménez (25), Pesquería (20)"
-                            " y Allende (19).",
+                        html.P(bulletedo4,
                            style={'font-size': 14, "font-family":"Arial", "text-align":"justify" }),
                ]), style={"background-color":"white",
                          "box-shadow": "10px 20px 30px black",
